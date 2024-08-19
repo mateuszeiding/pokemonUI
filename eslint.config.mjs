6f338c7162e590150dbff124d5dd7ceccca439cd
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-08-20 01:06:41 +0200

    Setup aliases

---
+++ eslint.config.mjs
@@ -0,0 +1,88 @@
+import { fixupConfigRules, fixupPluginRules } from '@eslint/compat';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import _import from 'eslint-plugin-import';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+export default [
+    {
+        ignores: ['**/dist', '**/.eslintrc.cjs'],
+    },
+    ...fixupConfigRules(
+        compat.extends(
+            'eslint:recommended',
+            'plugin:@typescript-eslint/recommended',
+            'plugin:react-hooks/recommended',
+            'prettier'
+        )
+    ),
+    {
+        plugins: {
+            'react-refresh': reactRefresh,
+            import: fixupPluginRules(_import),
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tsParser,
+        },
+
+        settings: {
+            react: {
+                version: 'detect',
+            },
+
+            'import/resolver': {
+                node: {
+                    paths: ['src'],
+                    extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
+                },
+
+                typescript: {
+                    project: './tsconfig.json',
+                },
+
+                alias: {
+                    map: [
+                        ['@', path.resolve(__dirname, './src')],
+                        [
+                            '@features',
+                            path.resolve(__dirname, './src/headless'),
+                        ],
+                        [
+                            '@headless',
+                            path.resolve(__dirname, './src/features'),
+                        ],
+                    ],
+
+                    extensions: ['.js', '.jsx', '.ts', '.d.ts', '.tsx'],
+                },
+            },
+        },
+
+        rules: {
+            'react-refresh/only-export-components': [
+                'warn',
+                {
+                    allowConstantExport: true,
+                },
+            ],
+        },
+    },
+];
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
+console.log('dirname', __dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+            '@headless': path.resolve(__dirname, './src/headless'),
+            '@features': path.resolve(__dirname, './src/features'),
+        },
+    },
+});
