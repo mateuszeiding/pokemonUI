00a64bcef0c95911c09ce6e1082fd3d285032a7e
MATEUSZ EIDING <eidingmateusz@gmail.com>
2024-08-19 21:36:23 +0200

    init

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+const config = {
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    quoteProps: 'as-needed',
+    jsxSingleQuote: true,
+    bracketSpacing: true,
+    bracketSameLine: true,
+    arrowParens: 'always',
+    htmlWhitespaceSensitivity: 'strict',
+    singleAttributePerLine: true,
+};
+
+export default config;
